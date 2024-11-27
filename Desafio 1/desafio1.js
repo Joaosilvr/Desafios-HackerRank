06fc7c69d0e06211ea7b8d7a10250285d10a4069
JOAO VITOR DA SILVA RAMOS <04644333042@senacrs.edu.br>
2024-11-27 19:42:53 -0300

    desafio 1 feito

---
+++ Desafio 1/desafio1.js
@@ -0,0 +1,16 @@
+function plusMinus(arr) {
+    let radiusCal = []
+    let calc = 0
+    for(let i = 0; i < arr.length;i++) {
+        calc = arr[i]/ 6.2830
+        radiusCal.push(calc)
+    }
+
+    for(let i = 0; i < radiusCal.length;i++) {
+        console.log(radiusCal[i].toFixed(6));
+    }
+}
+
+console.log(plusMinus([4,2,10]));
+
+// Este código recebe um array, a função é retornar o raio desses numeros
